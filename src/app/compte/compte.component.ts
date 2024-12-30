36c6c573e473027b78a5384101e957e42c857581
Ahmed hedef <ahmed50899128@gmail.com>
2024-12-30 23:00:33 +0100

    compte debiter function

--- src/app/compte/compte.component.ts
+++ src/app/compte/compte.component.ts
@@ -13,54 +13,61 @@ import { response } from 'express';
   styleUrl: './compte.component.css'
 })
 export class CompteComponent {
-    
-  comptes :any[]=[];
+
+  comptes: Compte[] = [];
   visible: boolean = false;
   editvisible: boolean = false;
   removevisible: boolean = false
   compteName!: string;
-  password!:string;
+  password!: string;
   isPasswordVisible: boolean = false;
-  selectedCompte: Compte = { id: 0, name: '', password:'',sold:0 };   
-
-
+  selectedCompte: Compte = { id: 0, name: '', password: '', solde: 0 };
+  debiterVisibilty:boolean=false;
+  montant:number=0;
+  passwordAverfier!:string;
   togglePasswordVisibility(): void {
     this.isPasswordVisible = !this.isPasswordVisible;
   }
   showDialog() {
     this.visible = true;
   }
-  showDialogEdit(compte:Compte) {
+  showDialogEdit(compte: Compte) {
     this.selectedCompte = { ...compte };
     this.editvisible = true;
   }
   showDialogRemove() {
-        this.removevisible = true;}
-  constructor(private listCompteservice: ListComptesService,private postCompte:PostCompteService,private putCompte:PutCompteService,private deleteCompte:DeleteCompteService,private confirmationService:ConfirmationService,private messageService:MessageService ) {}
-  loadservice(){
-          this.listCompteservice.getCompteWithAxios().then((data) => {
-            this.comptes = data;
-            console.log(data);
-            console.log(this.compteName);
-        });
-        }
+    this.removevisible = true;
+  }
+  showDialogDebiter(compte:Compte) {
+    this.selectedCompte = { ...compte };
+    this.debiterVisibilty = true;
+  }
+  constructor(private listCompteservice: ListComptesService, private postCompte: PostCompteService, private putCompte: PutCompteService, private deleteCompte: DeleteCompteService, private confirmationService: ConfirmationService, private messageService: MessageService) { }
+  loadservice() {
+    this.listCompteservice.getCompteWithAxios().then((data) => {
+      this.comptes = data;
+      console.log(data);
+      console.log(this.compteName);
+    });
+  }
   ngOnInit() {
     this.loadservice();
-    
-        }
 
-  addCompte(CompteName: string,Cpassword:string){
-    const newCompte={name: CompteName,password:Cpassword}
+  }
+
+  addCompte(CompteName: string, Cpassword: string) {
+    const newCompte = { name: CompteName, password: Cpassword }
     this.postCompte.createCompte(newCompte).subscribe(
       {
-        next:(response)=>console.log('Compte cree avec succes',response),
-        error: (error) => console.error('Erreur lors de la création du Compte:', error),});
-        this.visible = false
+        next: (response) => console.log('Compte cree avec succes', response),
+        error: (error) => console.error('Erreur lors de la création du Compte:', error),
+      });
+    this.visible = false
     this.loadservice();
     window.location.reload();
   }
 
-  updateCompte(compte:any): void {
+  updateCompte(compte: any): void {
     this.putCompte.updateCompte(compte).subscribe({
       next: (response) => {
         console.log('Compte mis à jour avec succès', response);
@@ -73,52 +80,77 @@ export class CompteComponent {
     this.loadservice();
     window.location.reload();
   }
-            delay(ms: number) {
-              return new Promise(resolve => setTimeout(resolve, ms));
-            }
+  delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
-            confirm2(event: Event, id: number) {
-              this.confirmationService.confirm({
-                target: event.target as EventTarget,
-                message: 'Are you sure you want to delete this Compte?',
-                icon: 'pi pi-exclamation-circle',
-                accept: () => {
-                  this.deleteCompte.deleteCompte(id).subscribe({
-                    next: (response) => {
-                      console.log('Compte supprimé avec succès', response);
-                      this.messageService.add({
-                        severity: 'success',
-                        summary: 'Deleted',
-                        detail: 'Compte has been deleted successfully.',
-                        life: 2000,
-                      })
-                      this.delay(3000);
-                      this.loadservice(); 
-                      window.location.reload();
-                    },
-                    error: (err) => {
-                      console.error('Erreur lors de la suppression du Compte', err);
-                      this.messageService.add({
-                        severity: 'error',
-                        summary: 'Error',
-                        detail: 'An error occurred while deleting the Compte.',
-                        life: 3000,
-                      });
-                    }
-                  });
-                },
-                reject: () => {
-                  this.messageService.add({
-                    severity: 'info',
-                    summary: 'Rejected',
-                    detail: 'You have rejected the deletion.',
-                    life: 3000,
-                  });
-                }
-              });
-            }
-        
-            
-        
-        }
-        
+  confirm2(event: Event, id: number) {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Are you sure you want to delete this Compte?',
+      icon: 'pi pi-exclamation-circle',
+      accept: () => {
+        this.deleteCompte.deleteCompte(id).subscribe({
+          next: (response) => {
+            console.log('Compte supprimé avec succès', response);
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Deleted',
+              detail: 'Compte has been deleted successfully.',
+              life: 2000,
+            })
+            this.delay(3000);
+            this.loadservice();
+            window.location.reload();
+          },
+          error: (err) => {
+            console.error('Erreur lors de la suppression du Compte', err);
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Error',
+              detail: 'An error occurred while deleting the Compte.',
+              life: 3000,
+            });
+          }
+        });
+      },
+      reject: () => {
+        this.messageService.add({
+          severity: 'info',
+          summary: 'Rejected',
+          detail: 'You have rejected the deletion.',
+          life: 3000,
+        });
+      }
+    });
+  }
+debite( ){
+  console.log(typeof this.selectedCompte.password);
+  console.log(typeof this.passwordAverfier);
+  if (this.montant > 0 &&  this.passwordAverfier === this.selectedCompte.password) {
+     const c1=this.comptes.find(c => c.id === this.selectedCompte.id );
+    if(c1){
+      c1.solde+=this.montant;
+    }
+    else{
+      console.log("compte not found")
+    }
+    
+  } else {
+    this.messageService.add({
+      severity: 'error',
+      summary: 'Échec',
+      detail: 'Montant invalide ou mot de passe incorrect.',
+      life: 2000,
+    })
+    console.log('Échec : montant invalide ou mot de passe incorrect.');
+  }
+this.debiterVisibilty=false;
+this.montant=0;
+this.passwordAverfier="";
+
+}
+
+
+}
+
--- src/app/compte/compte.ts
+++ src/app/compte/compte.ts
@@ -2,12 +2,12 @@ export class Compte {
     id: number;
     name: string;
     password: string;
-    sold: number;
+    solde: number;
   
     constructor(id_compte: number,nom: string ,password: string, capacite: number) {
       this.id= id_compte;
       this.password = password;
       this.name = nom;
-      this.sold = capacite;
+      this.solde = capacite;
       }
   }
